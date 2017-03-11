6c0be8556dfb80952d72a2c964a8eec15d9095e8
ErnestoPM <ernestopm20@gmail.com>
2017-03-11 01:57:04 -0600

    Adding new style and text editor

--- CodeRunner/app.js
+++ CodeRunner/app.js
@@ -1,5 +1,6 @@
 angular
   .module('codeRunner',
     [
+        'ui.codemirror'
     ])
   ;
--- CodeRunner/homeCtrl.js
+++ CodeRunner/homeCtrl.js
@@ -3,8 +3,21 @@ homeCtrl.$inject = ['$scope','$http'];
 
 function homeCtrl($scope,$http) {
 
+    $scope.code= "public class Test{\n" +
+        "   public static void main(String[] args){\n" +
+        "       System.out.println(\"Hello world!\");\n" +
+        "   }\n" +
+        "}" ;
+
+    $scope.editorOptions = {
+        lineWrapping : true,
+        lineNumbers: true,
+        mode: 'text/x-java',
+        theme: 'monokai'
+    };
+
   $scope.sendCode = function(){
-    console.log($scope.code);
+    //console.log($scope.code);
     $http.defaults.headers.post["Content-Type"] = "text/plain";
 
     $http.post('/',$scope.code).then(
